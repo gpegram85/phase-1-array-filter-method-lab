0e229ba95dc696d8ee627b01944877ba9070c91c
Grey Pegram <dpegram85@gmail.com>
2024-01-14 15:09:31 -0500

    Completed Labs

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(array, strQuery) {
+    let caseInsensitiveCheck = strQuery.toLowerCase()
+
+    const matchingDrivers = array.filter(function (name) {
+        return name.toLowerCase() === caseInsensitiveCheck
+    })
+    return matchingDrivers
+}
+
+function fuzzyMatch(array, strQuery) {
+    const matchingDrivers = array.filter(function (names) {
+        return names.startsWith(strQuery)
+    })
+    return matchingDrivers
+}
+
+function matchName(array, strQuery) {
+
+    const homeTown = array.filter(function (driver) {
+        return driver.name === strQuery
+    })
+    return homeTown
+}
